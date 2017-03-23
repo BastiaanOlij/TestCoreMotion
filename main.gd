b476293d555d7f1a40600fca0554716969e8141c
BastiaanOlij <mux213@gmail.com>
2017-03-23 18:30:40 +1100

    Horizon align magnetometer

--- main.gd
+++ main.gd
@@ -12,6 +12,10 @@ func _process(delta):
 	
 	var acc = Input.get_accelerometer()
 	var grav = Input.get_gravity()
+	if ((grav.x == 0.0) && (grav.y == 0.0) && (grav.z == 0.0)):
+		# No gravity? just use accelerometer, maybe one day add some math here to do something better
+		grav = acc
+
 	var useracc = acc - grav
 
 	text += "Accelerometer: " + str(acc.x).pad_decimals(2) + "   " + str(acc.y).pad_decimals(2) + "   " + str(acc.z).pad_decimals(2)
@@ -56,42 +60,49 @@ func _process(delta):
 		# normalize and transform gravity into world space
 		# note that our positioning matrix will be inversed to create our view matrix, so the inverse of that is our positioning matrix
 		# hence we can do:
-		grav = transform.basis.xform(grav)
-		text += "Adj grav: " + str(grav.x).pad_decimals(2) + "   " + str(grav.y).pad_decimals(2) + "   " + str(grav.z).pad_decimals(2) + "\n"
+		var grav_adj = transform.basis.xform(grav)
+		text += "Adj grav: " + str(grav_adj.x).pad_decimals(2) + "   " + str(grav_adj.y).pad_decimals(2) + "   " + str(grav_adj.z).pad_decimals(2) + "\n"
 		
 		# get rotation between our gravity and down vector
-		var dot = grav.dot(down)
+		var dot = grav_adj.dot(down)
 		if ((dot > -1.0) && (dot < 1.0)):
 			# axis around which we have this rotation
-			var axis = grav.cross(down)
+			var axis = grav_adj.cross(down)
 			axis = axis.normalized()
 
 			# adjust for drift
 			var rotate = Matrix3()
-			rotate = rotate.rotated(axis, -acos(dot))
+			rotate = rotate.rotated(axis, -acos(dot) * 0.2) # *0.2 to dampen it
 			transform.basis = rotate * transform.basis
 
-	# And do the same with our magnetometer (doesn't work on my iPhone so this is untested)
+	# And do something similar with our magnetometer
 	if ((magneto.x != 0.0) || (magneto.y != 0.0) || (magneto.z != 0.0)):
-		var north = Vector3(0.0, 0.0, -1.0)
+		# turns out the magnetometer is not horizon aligned so we need to combine it with our gravity
+		if ((grav.x != 0.0) || (grav.y != 0.0) || (grav.z != 0.0)):
+			var magneto_east = grav.cross(magneto) # or is this west?, but should be horizon aligned now
+			magneto_east = magneto_east.normalized()
+			magneto = grav.cross(magneto_east) # and now we have a horizon aligned north
+			magneto = magneto.normalized()
+		
+		var north = Vector3(0.0, 0.0, 1.0)
 		
 		# normalize and transform magneto into world space
-		magneto = transform.basis.xform(magneto)
-		text += "Adj magneto: " + str(magneto.x).pad_decimals(2) + "   " + str(magneto.y).pad_decimals(2) + "   " + str(magneto.z).pad_decimals(2) + "\n"
+		var magneto_adj = transform.basis.xform(magneto)
+		text += "Adj magneto: " + str(magneto_adj.x).pad_decimals(2) + "   " + str(magneto_adj.y).pad_decimals(2) + "   " + str(magneto_adj.z).pad_decimals(2) + "\n"
 		
 		# get rotation between our magneto and north vector
-		var dot = magneto.dot(north)
+		var dot = magneto_adj.dot(north)
 		if ((dot > -1.0) && (dot < 1.0)):
 			# axis around which we have this rotation
-			var axis = magneto.cross(north)
+			var axis = magneto_adj.cross(north)
 			axis = axis.normalized()
 
 			# adjust for drift
 			var rotate = Matrix3()
-			rotate = rotate.rotated(axis, -acos(dot))
+			rotate = rotate.rotated(axis, -acos(dot) * 0.2) # *0.2 to dampen it
 			transform.basis = rotate * transform.basis
 
-	# now that we have our orientation correct, let's use our accelerometer to move our camera
+	# now that we have our orientation correct, let's use our accelerometer to move our camera, this is not accurate enough... alas...
 	# useracc = transform.basis.xform(useracc)
 	# transform.origin += useracc * delta * movespeed
 
